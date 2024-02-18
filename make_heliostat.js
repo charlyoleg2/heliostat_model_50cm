ecf62a44c625475608634626e8647869b35b1d0b
charles <charles.braquet@e.email>
2024-02-18 08:20:49 +0100

    starting make_heliostat

---
+++ make_heliostat.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+// make_heliostat.js
+
+const parts = {
+	heliostat: 'heliostat_v01',
+	heliostat_2: 'heliostat_2_v01',
+	base: 'base_v01',
+	pole_static: 'pole_static_v01',
+	pole_rotor: 'pole_rotor_v01',
+	rake: 'rake_v01',
+	rake_stopper: 'rake_stopper_v01',
+	spider: 'spider_v01',
+	swing: 'swing_v01',
+	rod: 'rod_v01',
+	trapeze: 'trapeze_v01',
+	surface: 'surface_v01'
+};
+
+console.log('make_heliostat.js says Hello!');
+console.log('make_heliostat.js says Bye!');
